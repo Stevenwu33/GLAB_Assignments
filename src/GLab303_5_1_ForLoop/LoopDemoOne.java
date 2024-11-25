5d9ee8b3cea701425a64bc2ddbb4c3a83603e5bd
Steven Wu <stevenwu33@gmail.com>
2024-11-24 23:03:07 -0500

    GLAB - 303.5.1 - For loops

---
+++ src/GLab303_5_1_ForLoop/LoopDemoOne.java
@@ -0,0 +1,12 @@
+package GLab303_5_1_ForLoop;
+
+public class LoopDemoOne {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            System.out.println("Java is fun!");
+        }
+    }
+}
---
+++ src/GLab303_5_1_ForLoop/Main.java
@@ -0,0 +1,15 @@
+package GLab303_5_1_ForLoop;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int n = 1000;
+
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
