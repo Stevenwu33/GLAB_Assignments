eb7681a1824db0b5f703405b692854e50e88f020
Steven Wu <stevenwu33@gmail.com>
2024-12-02 16:35:16 -0500

    GLAB - 303.9.2- Inheritance

---
+++ src/GLab303_9_2_Inheritance/Hospital.java
@@ -0,0 +1,12 @@
+package GLab303_9_2_Inheritance;
+
+public class Hospital {
+
+    public static void main(String[] args) {
+
+        Surgeon s = new Surgeon();
+        s.Surgeon_Details();
+        s.Doctor_Details();
+
+    }
+}
---
+++ src/GLab303_9_2_Inheritance/Surgeon.java
@@ -0,0 +1,9 @@
+package GLab303_9_2_Inheritance;
+
+public class Surgeon extends Doctor{
+
+    void Surgeon_Details() {
+        System.out.println("Surgeon  Detail...");
+        System.out.println(Department = "Cardio");
+    }
+}
